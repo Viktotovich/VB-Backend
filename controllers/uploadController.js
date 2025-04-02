66c07bb5c4458666d5e82b1ea196b8046c78d624
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-04-02 23:34:17 +0400

    Added a security feature to use in Production (only) with caching enabled.
    Created TODOs for getting uploading images to work

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,5 +1,15 @@
+const db = require("../db");
+const multer = require("multer");
+
 module.exports.postUpload = (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
   res.json({ message: "fake success message", err: null });
 };
+
+/* TODOS: configure cloudinary, complete this function, and create a new File model that cascade with blog / user deletion
+
+async function processSubmitFile(req, res) {
+    const {originalname, encoding, mimetype, path, size} = req.file
+
+}
+
+*/
--- middleware/jwt/requireUser.js
+++ middleware/jwt/requireUser.js
@@ -5,3 +5,29 @@ module.exports.requireUser = (req, res, next) => {
 
   return next();
 };
+
+/* IMPLEMENT WITH CACHE ONLY!
+  -This improves security, but might eat up your db
+
+
+  const db = require("../../db");
+
+module.exports.requireUser = async (req, res, next) => {
+  if (!req.user) {
+    return res.status(403).json({ message: "Invalid Session" });
+  }
+
+  //My gut instincts felt like someone might try to attach a fake user
+  const isRealUser = await db.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+  });
+
+  if (!isRealUser) {
+    return res.status(403).json({ message: "Bonk, fake user" });
+  }
+
+  return next();
+};
+*/
