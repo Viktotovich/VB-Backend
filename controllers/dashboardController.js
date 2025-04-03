52d31bab411f91abbaddd173e06e19fb788d1045
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-04-03 19:26:14 +0400

    MAJOR BREAKTHROUGH: MVP for blog uploading done.
    UX/UI is abysmal, but works exactly how it should.
    
    Current version: "Third ko king ko"
    
    Post model upgraded, effectively removing authorId.
    
    Frontend is no longer compatible with v1.0.1
    
    Lots of TODOs left, however - the heaviest logic has been handled.
    
    Notably, close proximity tasks are:
    
    1 - To remove isTemp status from images if they are included in the blog
    
    2 - Run a cron job / auto job to clean isTemp true files.
    
    3 - Blog parser for the frontend
    
    4 - UX/UI improvements with due time

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,3 +1,39 @@
+const db = require("../db");
+
 module.exports.getDashboard = (req, res) => {
   res.json({ message: "success" });
+};
+
+module.exports.postBlog = async (req, res) => {
+  const build = req.body;
+  const userId = req.user.id;
+  const title = build.blocks[0].content;
+
+  console.log(req.body); //TODO: scan for file, make isTemp false if images included
+
+  //if there is a file included, make the file isTemp False
+  try {
+    await db.post.create({
+      data: {
+        title: title,
+        content: build,
+        categories: {
+          connectOrCreate: {
+            where: {
+              name: "No Category",
+            },
+            create: {
+              name: "No Category",
+            },
+          },
+        },
+        userId: userId,
+      },
+    });
+
+    res.json({ message: "success" });
+  } catch (err) {
+    console.error(err);
+    res.json({ message: "failed", error: err });
+  }
 };
--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -1,9 +1,11 @@
 const indexRouter = require("express").Router();
 const {
   getDashboard,
-  postLogin,
+  postBlog,
 } = require("../controllers/dashboardController");
 
 indexRouter.get("/", getDashboard);
+//TODO: set a rate limitor for blogs
+indexRouter.post("/blog", postBlog);
 
 module.exports = indexRouter;
